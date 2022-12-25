059bf5c241e4f7fdb2f62fc20c9e85782dd8e4c9
Kyle Morgan <kfwmorgan@gmail.com>
2022-12-25 14:22:56 -0600

    added logic to select game puzzle, conert str into list of char, take a user input and check to see if char is in list of game puzzle, add user choices to list for tracking, aswell as handling removal of char from list if users guess is in the puzzle.

--- main.py
+++ main.py
@@ -1,5 +1,45 @@
-print ('hello world')
+import random
+import math
+
+puzzles = []
+usersSelections = []
+gameStatus = True
+wrongGuess = 0 
+
+#reads puzzle file 
 file = open('puzzles.txt', 'r')
-read_file = file.readlines()
-print (read_file)
+puzzles = file.readlines()
 file.close()
+
+#select a random puzzle from puzzles.txt to be the game puzzle and splits the stirng into list of characters
+puzzleChoice = math.floor(random.random() * len(puzzles))
+selectedPuzzle = puzzles[puzzleChoice].strip()
+gamePuzzle = list(selectedPuzzle)
+
+while gameStatus == True:
+    puzzleLength = len(gamePuzzle)
+    print(gamePuzzle)
+    playerGuess = input('Select a letter to guess:')
+    if playerGuess not in usersSelections:
+        usersSelections.append(playerGuess)
+        if playerGuess in gamePuzzle:
+            gamePuzzle.remove(playerGuess)
+            for i in range (puzzleLength):
+                if playerGuess in gamePuzzle:
+                    gamePuzzle.remove(playerGuess)
+                    print(gamePuzzle)
+            puzzleLength = len(gamePuzzle)
+            if puzzleLength == 0:
+                print("You win Game Over!")
+                gameStatus = False
+        else:
+            wrongGuess +=1
+            print ("Choose another letter!")
+            if wrongGuess == 3:
+                print('Game Over: You Lose!')
+                gameStatus = False
+    else:
+        playerGuess = input('Letter has been chosen already, Choose again:')
+
+
+
